732f8ec19d25a39351396db3d2f6d56f4e174b02
kokolenay <kk4915884@gmail.com>
2024-08-23 10:03:43 +0800

    test_push

---
+++ Student_project/src/main/java/com/cqu/student_project/pojo/Student.java
@@ -0,0 +1,18 @@
+package com.cqu.student_project.pojo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int id;
+    private String stuName;
+    private String college;
+    private String major;
+    private String idCard;
+    private String classes;
+    private String stuId;
+    private String phone;
+    private String password;
+    private int status;
+    private String img;
+}
